b51aed2b4368f9ce4ce9f3853b44d3f4cc1fbdc8
John Ahrens <john@john-ahrens.com>
2008-12-14 19:59:47 -0700

    Add new Gravity class and Gravity.plist

---
+++ Gravity.h
@@ -0,0 +1,17 @@
+//
+//  Gravity.h
+//  imSimReal
+//
+//  Created by John Ahrens on 12/14/08.
+//  Copyright 2008 John Ahrens, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Gravity : NSObject 
+{
+
+}
+
+@end
---
+++ Gravity.m
@@ -0,0 +1,14 @@
+//
+//  Gravity.m
+//  imSimReal
+//
+//  Created by John Ahrens on 12/14/08.
+//  Copyright 2008 John Ahrens, LLC. All rights reserved.
+//
+
+#import "Gravity.h"
+
+
+@implementation Gravity
+
+@end
