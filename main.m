061d7e7e27365f664698c8b6b5b43a2aba0a4aa1
John Ahrens <john@john-ahrens.com>
2009-06-04 15:02:05 -0700

    Add files

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  imSimReal
+//
+//  Created by John Ahrens on 12/21/08.
+//  Copyright John Ahrens, LLC 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
